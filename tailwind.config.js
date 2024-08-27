adba5f6c2bcab4800dd00c2390ef9c83586c9672
Bilek-Petr <p.bilek29@gmail.com>
2024-08-27 18:55:19 +0200

    setup libraries and variables

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,7 +14,6 @@ export default {
 
       container: {
         center: true,
-        // padding: '1rem'
       },
       boxShadow: {
         navShadow: "0 0 5px 0 rgba(0, 0, 0, .2)",
@@ -25,6 +24,15 @@ export default {
           "0 35px 35px rgba(0, 0, 0, 0.25)",
           "0 45px 65px rgba(0, 0, 0, 0.15)",
         ],
+      },
+      keyframes: {
+        slideDown: {
+          "0%": { transform: "translateY(-100%)", opacity: "0" },
+          "100%": { transform: "translateY(0)", opacity: "1" },
+        },
+      },
+      animation: {
+        slideDown: "slideDown 0.5s ease-out",
       },
     },
   },
