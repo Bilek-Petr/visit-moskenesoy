dca732502dcdabc2d67a5a2a3972dddb62d75ca9
Bilek-Petr <p.bilek29@gmail.com>
2024-09-03 11:42:29 +0200

    finished version of textBanner

--- src/components/Shared/TextBanner.jsx
+++ src/components/Shared/TextBanner.jsx
@@ -1,18 +1,25 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import { RiDoubleQuotesL } from "react-icons/ri";
+import { useScroll } from "../../context/ScrollContext";
 
 export default function TextBanner() {
+  const { bannerTextRef } = useScroll();
+
   return (
-    <div className="relative m-auto w-11/12 max-w-3xl space-y-4 px-8 py-40">
+    <div
+      id="bannerText"
+      ref={bannerTextRef}
+      className="relative m-auto w-11/12 max-w-3xl space-y-4 px-8 py-40"
+    >
       <RiDoubleQuotesL className="absolute left-0 top-0 size-20 translate-y-[9rem] transform text-mainRed opacity-30" />
       <p className="relative z-10 text-5xl sm:text-7xl">
         Welcome to Moskonesøy
       </p>
-      <p className="text-fontGrey font-raleway font-medium italic">
+      <p className="font-raleway font-medium italic text-fontGrey">
         a small paradise in Norway’s Lofoten
       </p>
       <hr />
-      <p className="text-fontGrey font-raleway font-medium leading-7">
+      <p className="font-raleway font-medium leading-7 text-fontGrey">
         Experience breathtaking landscapes where rugged peaks meet
         crystal-fjords waters. Explore scenic trails, enjoy tranquil kayaking,
         and savor fresh seafood in a charming setting. Embrace the warmth of
